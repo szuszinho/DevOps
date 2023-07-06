49a8e356faf8bebca738310b057fb7a4e9541cca
szuszinho <szuszinho@outlook.hu>
2023-07-06 14:41:26 +0200

    del ssh-key

--- Terraform/lxc_postgres.tf
+++ Terraform/lxc_postgres.tf
@@ -23,9 +23,7 @@ resource "proxmox_lxc" "db_postgres" {
   swap = 0
   tags = "Postgres K3s etcd"
   vmid = 399
-  ssh_public_keys = <<-EOT
-    ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDTdHVoepiv/XsNujNjyOThQy7qu9lDxJvwbGZhEED6UcpEj3wHU80I/RGicbFbgsZhtit7Xr1R64wSMh5XyR4XukvKZ/E/mP5ife89+rC9hCLZwGrVwP/xYCcxJC0FYufYzvkoekdxHIaU6pHQTRSXvK0R1nL+fnQcLtVeGQw2G7FOZnSJqB1Xj2cMARKz/1tlWHy0tyWtkEPCeR2eHrsXZXQvmv+FuPO7wYJxElNlB6s10Kywd9m+jm9KprKddxRLYZGSavDi1OqGB+SRIuedH/0Cg+hwSlll8IRp9aFwvL1U6v4AT+SSoY8gAne6WBDyVjTplgJzLVm2/K9SCBwqhHssqeezBdlwCmwFb48ycQ0en3kDAtH/cGxUmxgr4GMJZPUcPsX3swJ2RrJnu/HXdcZViXM/bRGMuCi04yHpvjTTKuNt/g+IV2ZsI6arKDBPFe6wPNtwAenfq1QXo/SwL4cMV5oZaNRPQ1tmMRQ5JyCyIQ95oD7z7AXADI49uZM= szuszi@Win10-PC
-  EOT
+  ssh_public_keys = ""
 
   rootfs {
     storage = "storage"
